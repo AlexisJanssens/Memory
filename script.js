9cb107ec4ce74bcc256cfe51913634c57aaaaa13
Alexis Janssens <Alexisjanssens@hotmail.fr>
2023-08-31 11:50:16 +0200

    Final version

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// All avaibles cards
 const cards = [
     { number: 0, name: "Jeremy", url: "./images/jeremy.jpg" },
     { number: 1, name: "Nicolas", url: "./images/nico.png" },
@@ -16,12 +17,34 @@ const cards = [
 ]
 console.log(cards)
 
+// init variables
+
 let deck = []
 let indexCard = 0
-let lastIndex
 let guess = ""
 let revealedCards = []
 let flippedCard = []
+let starterDate = new Date()
+let timer = document.getElementById("timer")
+let table = document.getElementById("memoryContent")
+let difficulty = document.getElementById("difficulty")
+let reset = document.getElementById("reset")
+
+// Timer
+
+function showTime() {
+    timer.innerHTML = millisToMinutesAndSeconds(new Date() - starterDate)
+}
+
+function millisToMinutesAndSeconds(millis) {
+    let minutes = Math.floor(millis / 60000)
+    let seconds = ((millis % 60000) / 1000).toFixed(0)
+    return minutes + ":" + (seconds < 10 ? "0" : "") + seconds
+}
+
+setInterval(showTime, 1000)
+
+// get the specified nmber of cards (but they are semi-ordered)
 
 function getCards(maxCards) {
     for (let i = 0; i < maxCards / 2; i++) {
@@ -34,6 +57,8 @@ function getCards(maxCards) {
         }
     }
 }
+
+// shuffle those cards
 
 function shuffle(array) {
     let currentIndex = array.length,
@@ -51,9 +76,7 @@ function shuffle(array) {
     return array
 }
 
-function flipcard(cardContent) {
-    cardContent.classList.toggle("flipCard")
-}
+// distribute cards for the game
 
 function distribute() {
     for (let card in deck) {
@@ -85,32 +108,37 @@ function distribute() {
     }
 }
 
+// toggle the flipcard animation
+
 function flipcard(cardContent, revealedCard, card) {
     if (
         flippedCard.find((elem) => elem == card) ||
-        revealedCard.find((elem) => elem == deck[card].name)
+        revealedCard.find((elem) => elem == deck[card].name) ||
+        flippedCard.length == 2
     ) {
         console.log("pas content")
     } else {
         cardContent.classList.toggle("flipCard")
-        flippedCard.push(card)
     }
 }
 
-let table = document.getElementById("memoryContent")
-let difficulty = document.getElementById("difficulty")
+// reset the game and timer on the reset button
 
-difficulty.addEventListener("change", () => {
-    if (difficulty.value == "easy") {
+function resetAll() {
+    if (flippedCard.length == 2) {
+        console.log("attendez la fin de la manche")
+    } else if (difficulty.value == "easy") {
         resetGame()
         getCards(12)
         shuffle(deck)
         distribute()
+        starterDate = new Date()
     } else if (difficulty.value == "normal") {
         resetGame()
         getCards(18)
         shuffle(deck)
         distribute()
+        starterDate = new Date()
 
         console.log("normal")
     } else if (difficulty.value == "hard") {
@@ -119,10 +147,41 @@ difficulty.addEventListener("change", () => {
         shuffle(deck)
         distribute()
         console.log("hard")
+        starterDate = new Date()
+    }
+}
+reset.addEventListener("click", resetAll)
+
+// change the difficulty
+
+difficulty.addEventListener("change", () => {
+    if (difficulty.value == "easy") {
+        resetGame()
+        getCards(12)
+        shuffle(deck)
+        distribute()
+        starterDate = new Date()
+    } else if (difficulty.value == "normal") {
+        resetGame()
+        getCards(18)
+        shuffle(deck)
+        distribute()
+        starterDate = new Date()
+
+        console.log("normal")
+    } else if (difficulty.value == "hard") {
+        resetGame()
+        getCards(24)
+        shuffle(deck)
+        distribute()
+        console.log("hard")
+        starterDate = new Date()
     } else {
         resetGame()
     }
 })
+
+// delete "lastGuess" className
 
 function deleteLastGuess() {
     document
@@ -130,12 +189,13 @@ function deleteLastGuess() {
         .classList.remove("lastGuess")
 }
 
+// toggle the flip card on the previous guess
+
 function resetImg() {
-    // document.getElementsByClassName("lastGuess")[0].style.backgroundColor =
-    //     "yellow"
-    // document.getElementsByClassName("lastGuess")[0].style.backgroundImage = ""
     document.getElementsByClassName("lastGuess")[0].classList.toggle("flipCard")
 }
+
+// reset HTML for discoverd cards
 
 function resetHTML(back, lastBack) {
     back.style.backgroundColor = "yellow"
@@ -144,46 +204,60 @@ function resetHTML(back, lastBack) {
     lastBack.style.backgroundImage = ""
 }
 
+// flip cards, reset HTML prepare for next guess
+
 function resetCards(cardHTML, back) {
     let lastBack = document.querySelector(".lastGuess .back")
     console.log(lastBack)
-    setTimeout(() => resetHTML(back, lastBack), 1000)
+    setTimeout(() => resetHTML(back, lastBack), 400)
 
     cardHTML[0].classList.toggle("flipCard")
     document.getElementsByClassName("lastGuess")[0]
     resetImg()
 
     deleteLastGuess()
+    flippedCard = []
     console.log("go")
+    console.log(flippedCard)
 }
+
+// reset all variables for new game
 
 function resetGame() {
     table.innerHTML = ""
     deck = []
     revealedCards = []
     guess = ""
+    flippedCard = []
 }
+
+// show back of cards and decide what need to do if it's a match or not
 
 function revealCard(card, back) {
     console.log(card)
     let cardHTML = document.getElementsByClassName("card" + card)
     back.style.backgroundColor = "transparent"
     back.style.backgroundImage = "url(" + deck[card].url + ")"
-    if (revealedCards.find((element) => element == deck[card].name)) {
+    if (flippedCard.length == 2) {
+        console.log("wait")
     } else if (guess == "") {
         guess = deck[card].name
         cardHTML[0].classList.add("lastGuess")
         lastIndex = card
+        flippedCard.push(card)
     } else if (deck[card].name == guess && card != lastIndex) {
         revealedCards.push(guess)
         guess = ""
         deleteLastGuess()
+        flippedCard = []
     } else if (card == lastIndex) {
         console.log("veuillez choisir un carte différente")
     } else {
         guess = deck[card].name
         guess = ""
-        flippedCard = []
-        setTimeout(() => resetCards(cardHTML, back), 1000)
+        flippedCard.push(card)
+
+        setTimeout(() => resetCards(cardHTML, back), 800)
+        console.log(flippedCard)
     }
 }
